ede0ae3864213f4985385405b22fb08ffa5ec7ae
Julyjujuy <tantissimo.entusiasmo@gmail.com>
2022-07-10 17:32:22 +0200

    Add project files.

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Floor Width");
+            string userInputWidth = Console.ReadLine();
+            int NewUserInputWidth = Convert.ToInt32(userInputWidth);
+
+            Console.WriteLine("Enter Floor Lenght");
+            string userInputLenght = Console.ReadLine();
+            int NewUserInputLenght = Convert.ToInt32(userInputLenght);
+
+            Console.WriteLine("Enter Cost per Unit");
+            string userInputCost = Console.ReadLine();
+            int NewUserInputCost = Convert.ToInt32(userInputCost);
+
+            int squareMeters = NewUserInputWidth * NewUserInputLenght;
+            int CostOfFlooring = squareMeters * NewUserInputCost;
+
+            Console.WriteLine(CostOfFlooring);
+            Console.ReadLine();
+
+        }
+    }
+}
