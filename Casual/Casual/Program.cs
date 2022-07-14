5351cbfd22c0a5704cb9e8686ba2f388afc577b3
Julyjujuy <tantissimo.entusiasmo@gmail.com>
2022-07-14 20:36:06 +0200

    tried with a while loop

---
+++ Casual/Casual/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //read the string
+            Console.WriteLine("Enter String to Reverse Please");
+            string s = Console.ReadLine();
+            //convert string to array
+            char[] chars = s.ToCharArray();
+            //Reverse Array
+            Array.Reverse(chars);
+            //Display Result
+            Console.WriteLine(chars);
+            //Pause
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ReverseStringImproved/ReverseStringImproved/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseStringImproved
+{
+    internal class Program
+    {
+        static void Main(string[] stringInput)
+        {
+            string Str, Revstr = ""; 
+            
+
+            int Length; 
+            
+
+            Console.Write("Please Enter A String: "); 
+            
+
+            Str = Console.ReadLine(); 
+            
+
+            Length = Str.Length - 1; 
+            
+
+            while (Length >= 0)  
+                
+            {
+                Revstr = Revstr + Str[Length];  
+                Length--;
+            }
+            Console.WriteLine("Reverse  String  Is  {0}", Revstr); 
+           
+
+            Console.ReadLine();  
+           
+
+        }
+    }
+}
